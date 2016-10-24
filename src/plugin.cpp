92b92ad3aea8ceb02b8caf754b20f7ac34cd3ec1
FrankYW_Chen <FrankYW_Chen@compal.com>
2016-10-24 13:38:29 +0800

    able to link the service command in gazebo plugin

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -5,18 +5,72 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/gazebo.hh>
 #include <urdf/model.h>
+#include <compal_gazebo/SetModelPoseService.h>
 
 namespace gazebo {
 
-class AGV2GazeboPlugin : public ModelPlugin {
+const std::string SetModelPoseServiceName = "SetModelPoseService";
+
+// ========================================================================== //
+
+class ServiceManager {
 public:
-  AGV2GazeboPlugin() {}
+  ServiceManager() : is_called_(false) {}
+  virtual void CallBack() = 0;
+  void SetCalledFlag() { is_called_ = true; }
+private:
+  bool is_called_;
+};
+
+// ========================================================================== //
+
+template <typename...> class ServicePackage;
+
+template <typename SvrReq, typename SvrRes>
+class ServicePackage<SvrReq, SvrRes> : public ServiceManager {
+public:
+  ServicePackage(std::string service_name) : ServiceName_(service_name) {}
+  // virtual ~ServicePackage();
+  bool Init(ros::NodeHandle* nh);
+  bool ServerCallback(SvrReq &req, SvrRes &res);
+  void CallBack();
+  std::string GetServiceName() { return ServiceName_; };
+private:
+  std::string ServiceName_;
+  boost::shared_ptr<ros::NodeHandle> rosnode_;
+  ros::ServiceServer srv_server_;
+};
+template <typename SvrReq, typename SvrRes>
+bool ServicePackage<SvrReq, SvrRes>::Init(ros::NodeHandle* nh) {
+  rosnode_.reset(nh);
+  ROS_INFO_STREAM("ready for adv new service called: " << ServiceName_ );
+  srv_server_ = rosnode_->advertiseService(ServiceName_, &ServicePackage::ServerCallback, this);
+  return true;
+}
+template <typename SvrReq, typename SvrRes>
+bool ServicePackage<SvrReq, SvrRes>::ServerCallback(SvrReq &req, SvrRes &res) {
+  ROS_INFO_STREAM("x: " << req.x << ", " << "y: " << req.y << ", " << "x: " << req.z);
+  res.result = "data passed";
+  return true;
+}
+template <typename SvrReq, typename SvrRes>
+void ServicePackage<SvrReq, SvrRes>::CallBack() { return; }
+
+// ========================================================================== //
+
+class AGV2GazeboPlugin : public ModelPlugin {
+  typedef std::vector<ServiceManager> ServiceManagerVec;
+  typedef std::vector<ServiceManager>::iterator ServiceManagerVecIter;
+public:
+  AGV2GazeboPlugin() : set_model_pose_service_(SetModelPoseServiceName) {}
   ~AGV2GazeboPlugin() {}
   virtual void Load(physics::ModelPtr parent, sdf::ElementPtr sdf);
   virtual void Init();
 
 private:
   void OnUpdate(const common::UpdateInfo& info);
+
+  ServicePackage<compal_gazebo::SetModelPoseService::Request, compal_gazebo::SetModelPoseService::Response> set_model_pose_service_;
 
   physics::Link_V links_;
   physics::ModelPtr model_;
@@ -28,6 +82,7 @@ private:
   ros::Time last_update_time_;
   ros::NodeHandle nh_;
   ros::Time last_publish_;
+  ServiceManagerVec service_list_;
 };
 
 void AGV2GazeboPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
@@ -44,6 +99,7 @@ void AGV2GazeboPlugin::Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
       sdf->GetElement("robotNamespace")->Get<std::string>();
   }
   rosnode_.reset(new ros::NodeHandle(robot_namespace_));
+  set_model_pose_service_.Init(rosnode_.get());
 }
 
 void AGV2GazeboPlugin::Init() {
